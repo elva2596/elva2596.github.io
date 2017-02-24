2adc53213636cbe1c13127d5ce6b2d0b73943c66
elva2596 <646994014@qq.com>
2017-02-24 17:58:51 +0800

    自定义menu

--- ife-tasks/task-2/index.js
+++ ife-tasks/task-2/index.js
@@ -2,35 +2,37 @@ window.onload = function (){
   var oClick = document.getElementById('click_region'),
       oMenu = document.getElementById('menu'),
       aLi = oMenu.getElementsByTagName('li'),
-      browserHeight = window.innerHeight,
-      browserWidth = window.innerWidth;
+      browserHeight = document.documentElement.clientHeight,//浏览器视口的高度
+      browserWidth = document.documentElement.clientWidth; //浏览器视口的快读，不包括垂直滚动天的宽度
+
   document.oncontextmenu = function (ev){
+    oMenu.style.display = 'block';
     var ev = ev||window.event,
         scrollTop = document.documentElement.scrollTop||document.body.scrollTop,
         scrollLeft = document.documentElement.scrollLeft||document.body.scrollLeft,
         clientX = ev.clientX,
         clientY = ev.clientY,
+        // 注意：只有在隐藏的元素变成display:block的状态才能获取他的宽度和高度
         offsetWidth = oMenu.offsetWidth,
         offsetHeight = oMenu.offsetHeight,
         top,
         left;
 
-    oMenu.style.display = 'block';
-
-    if(clientY+offsetHeight>=browserHeight){
+    if(clientY+offsetHeight>browserHeight){
        top = clientY-offsetHeight
     }else{
       top = clientY
     }
 
-    if(clientX+offsetWidth>=browserWidth){
+    if(clientX+offsetWidth>browserWidth){
       left = clientX-offsetWidth
+      console.log(left);
     }else{
       left = clientX
     }
 
     oMenu.style.left = left+'px';
-    oMenu.style.top = top+'px';
+    oMenu.style.top =scrollTop+top+'px';
     return false//阻止默认行为，并且阻止冒泡
   }
 
