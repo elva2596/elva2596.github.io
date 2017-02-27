00b039c66db10331287a3d030c312e4c54298b91
elva2596 <646994014@qq.com>
2017-02-27 15:18:35 +0800

    动态数据绑定一

--- ife-tasks/task-4/index.js
+++ ife-tasks/task-4/index.js
@@ -13,14 +13,15 @@ p.walk = function (obj){
 		_this = this;
 	Object.keys(obj).forEach(function (key){
 		val = obj[key]
-		_this.observer(val)
+		if(typeof val==="object"){
+			new Observer(val)
+		}
 		_this.convert(key,val)
 	})
 }
 
 // 绑定getter 和setter
 p.convert = function (key,val){
-	var _this = this
 	Object.defineProperty(this.data,key,{
 		configurable:true,
 		enumarable:true,
@@ -31,19 +32,17 @@ p.convert = function (key,val){
 		set:function (newVal){
 			console.log("你设置了"+key);
 			console.log("新的"+key+"="+newVal);
+
 			// 如果设置的新值是一个对象，则递归它，加上set/get
-			_this.observer(newVal)
+			if(typeof newVal ==="object"){
+				new Observer(newVal);
+			}
+
 			val = newVal
 		}
 	})
 }
 
-p.observer = function (value){
-	if (!value || typeof value !== 'object') {
-    return
-  }
-  return new Observer(value)
-}
 let data = {
     user: {
         name: "hello world",
